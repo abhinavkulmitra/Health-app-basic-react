fd8dbf494fad2daeafe002bbeb1fb243ba267d3e
Abhinav Kulmitra <47947165+abhinavkulmitra@users.noreply.github.com>
2020-12-28 23:22:34 +0530

    added a new module for components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import React from "react";
 import "./styles.css";
-import Food from "./food";
 import food from "./data/database";
-
-const Header = () => <h2>Health App</h2>; //header arrow-function
-
-const ListOfFood = ({ array, foodPillHandler, userCalorie }) =>
-  array.map(({ name, calories, price }) => (
-    <Food
-      key={name}
-      fooditemName={name}
-      fooditemCalories={calories}
-      fooditemPrice={price}
-      onFoodPillClick={foodPillHandler}
-      totalCalorieIntake={userCalorie}
-    />
-  ));
-const CalorieOutput = ({ calorieState, statement }) => (
-  <div>
-    <h4>{statement}</h4>
-    <h2>{calorieState}</h2>
-  </div>
-);
+import { Header, ListOfFood, CalorieOutput } from "./pageOne";
+import router from "react-router-dom";
 
 export default class App extends React.Component {
   state = {
